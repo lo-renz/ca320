be8871ce3cee20cedb769973a5abc8409552c8dd
Renso Guilalas <rensoglorenzo@gmail.com>
2023-10-27 15:44:15 +0100

    Working through 'Learn You a Haskell' notes.

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -12,3 +12,6 @@ length' xs = sum [1 | _ <- xs]
 
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
