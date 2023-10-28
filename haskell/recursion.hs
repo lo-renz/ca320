bac5801e056f8c4b28dfb10a715ec1f13d50519b
Renso Guilalas <rensoglorenzo@gmail.com>
2023-10-28 21:20:18 +0100

    Working through 'Learn You a Haskell' notes.

---
+++ haskell/recursion.hs
@@ -0,0 +1,47 @@
+-- finding the biggest number in a list
+{-maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs-}
+
+-- more elegant way
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+-- replicate x times a number
+-- replicate 3 5 returns [5, 5, 5]
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n<= 0 = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ []   = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+-- zip [1,2,3] [2,3] returns [(1,2),(2,3)] because it truncates the longer list
+-- to match the length of the shorter one.
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem` xs
