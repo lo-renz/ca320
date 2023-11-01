c4ddfa429dd823de996fe5635ed20bc41b62a642
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-11-01 19:01:32 +0000

    Working through the 'Learn You a Haskell notes'.

--- haskell/higher-order-functions.hs
+++ haskell/higher-order-functions.hs
@@ -12,3 +12,58 @@ isUpperAlphaNum = (`elem` ['A'..'Z'])
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- flip' :: (a -> b -> c) -> (b -> a -> c)
+-- flip' f = g
+--     where g x y = f y x
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+{- Quicksort using filter -}
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter' (<=x) xs)
+        biggerSorted = quicksort (filter' (>=x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter' p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+sumOddSquares :: Int
+sumOddSquares = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+sumEvenSquares :: Int
+sumEvenSquares = sum (takeWhile (<10000) (filter even (map (^2) [1..])))
+
+{- Collatz Sequences -}
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain (n*3 + 1)
+
+-- For all starting numbers from 1 to 100, how many chains have a length greater than 15?
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
