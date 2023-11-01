4f6429a71521eec9135bf700b8d4c6ee380f767a
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-11-01 09:17:18 +0000

    Some changes.

--- lab_exams/2021.hs
+++ lab_exams/2021.hs
@@ -9,3 +9,13 @@ sumOrDiff :: Int -> Int -> Int
 sumOrDiff x y = if x `mod` 2 == 1 || y `mod` 2 == 1
                   then x + y
                 else x - y
+
+-- Q3
+-- Write a function, join, that takes two lists and returns a list that is the concatenation of both lists except the elements in the
+-- first list that are already in the second list are not added to the resulting list, e.g. join [1,2,3] [2,4,6,8] evaluates to
+-- [1,3,2,4,6,8]
+join :: [Int] -> [Int] -> [Int]
+join [] ys = ys
+join (x:xs) ys
+    | x `elem` ys = join xs ys
+    | otherwise = x : join xs ys
