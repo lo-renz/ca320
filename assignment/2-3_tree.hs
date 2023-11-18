f9b3b05290a5f2d5e025cabf81a1c297bacd5979
Renso Guilalas <rensoglorenzo@gmail.com>
2023-11-18 23:42:12 +0000

    Added comments.

--- assignment/2-3_tree.hs
+++ assignment/2-3_tree.hs
@@ -1,13 +1,10 @@
 {-
- A 2-3 Tree which is either:
+ A datatype which depicts that a 2-3 Tree can either be:
  - An empty tree
 
- - A node with 1 root element, a left subtree in which all the elements are less than or equal to the root element,
-   and a right subtree in which all the elements are greater than the root element.
+ - A node with 1 root element and two subtrees.
 
- - A node with 2 root elements, a left subtree in which all the elements are less than or equal to the first root element,
-   a middle subtree in which all the elements are greater than the first root element and less than or equal to the second
-   root element, and a right subtree in which all the elements greater than the second root element.
+ - A node with 2 root elements and three subtrees.
 -}
 data TwoThreeTree t = Empty
                     | TwoNode t (TwoThreeTree t) (TwoThreeTree t)
@@ -15,13 +12,14 @@ data TwoThreeTree t = Empty
                     deriving (Eq, Ord, Show)
 
 
--- Tree from assignment spec can be respresented as follows:
-myTree1 = ThreeNode 3 10 Empty (TwoNode 6 Empty Empty) (ThreeNode 15 20 Empty (TwoNode 18 Empty Empty) Empty)
+-- Using the above datatype the tree from assignment spec can be respresented as follows:
+--myTree1 = ThreeNode 3 10 Empty (TwoNode 6 Empty Empty) (ThreeNode 15 20 Empty (TwoNode 18 Empty Empty) Empty)
 
 twoLeaf x = TwoNode x Empty Empty
 threeLeaf x y = ThreeNode x y Empty Empty Empty
 
-myThree2 = ThreeNode 3 10 Empty (twoLeaf 6) (ThreeNode 15 20 Empty (twoLeaf 18) Empty)
+-- Using the above, we can rewrite the tree as:
+--myThree2 = ThreeNode 3 10 Empty (twoLeaf 6) (ThreeNode 15 20 Empty (twoLeaf 18) Empty)
 
 -- Implementation of add(X, T), which returns the 2-3 Tree from adding X to the 2-3 Tree T.
 {- Explanation:
