d372ec4295444f309570bebfe92dd596815bd323
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-11-19 01:48:29 +0000

    Formatted the code and added comments.

--- assignment/2-3_tree.hs
+++ assignment/2-3_tree.hs
@@ -6,34 +6,47 @@
 
  - A node with 2 root elements and three subtrees.
 -}
-data TwoThreeTree t = Empty
-                    | TwoNode t (TwoThreeTree t) (TwoThreeTree t)
-                    | ThreeNode t t (TwoThreeTree t) (TwoThreeTree t) (TwoThreeTree t)
-                    deriving (Eq, Ord, Show)
-
+data TwoThreeTree t
+  = Empty
+  | TwoNode t (TwoThreeTree t) (TwoThreeTree t)
+  | ThreeNode t t (TwoThreeTree t) (TwoThreeTree t) (TwoThreeTree t)
+  deriving (Eq, Ord, Show)
 
 -- Using the above datatype the tree from assignment spec can be respresented as follows:
---myTree1 = ThreeNode 3 10 Empty (TwoNode 6 Empty Empty) (ThreeNode 15 20 Empty (TwoNode 18 Empty Empty) Empty)
+-- myTree1 = ThreeNode 3 10 Empty (TwoNode 6 Empty Empty) (ThreeNode 15 20 Empty (TwoNode 18 Empty Empty) Empty)
 
 twoLeaf x = TwoNode x Empty Empty
+
 threeLeaf x y = ThreeNode x y Empty Empty Empty
 
 -- Using the above, we can rewrite the tree as:
---myThree2 = ThreeNode 3 10 Empty (twoLeaf 6) (ThreeNode 15 20 Empty (twoLeaf 18) Empty)
+-- myThree2 = ThreeNode 3 10 Empty (twoLeaf 6) (ThreeNode 15 20 Empty (twoLeaf 18) Empty)
 
 -- Implementation of add(X, T), which returns the 2-3 Tree from adding X to the 2-3 Tree T.
 {- Explanation:
+  A function which adds a value to a specified tree. It uses pattern matching to check whether the tree is
+  empty, a 2-node or 3-node tree and acts accordingly.
+
+  If an element is being added to an empty tree, then the returned tree is a 2-node tree which contains the
+  added element and two empty subtrees.
+
+  For elements added to a 2-node tree, depending on the element being added to the 2-node tree, the smaller
+  element is added to the left while the bigger element is added to the right.
+
+  I use recursion to add elements to a 3-node tree. This is done depending on the element being added. Let's
+  define x to be the element added to the tree t. Let's also define y and z to be the elements found in the
+  tree t in respective order. If x <= y then the element is added to the left subtree. If y < x <= z then the
 -}
 add :: (Ord t) => t -> TwoThreeTree t -> TwoThreeTree t
 add x Empty = twoLeaf x
 add x (TwoNode y left right)
-    | x < y     = ThreeNode x y left Empty right
-    | otherwise = ThreeNode y x left Empty right
+  | x < y = ThreeNode x y left Empty right
+  | otherwise = ThreeNode y x left Empty right
 add x (ThreeNode y z left middle right)
-    | x <= y          = ThreeNode y z (add x left) middle right
-    | x > y && x <= z = ThreeNode y z left (add x middle) right
-    | x > z           = ThreeNode y z left middle (add x right)
-    | otherwise = error "Something wrong happened"
+  | x <= y = ThreeNode y z (add x left) middle right
+  | x > y && x <= z = ThreeNode y z left (add x middle) right
+  | x > z = ThreeNode y z left middle (add x right)
+  | otherwise = error "Something wrong happened"
 
 -- Implementation of member(X, T), which returns true if X is in the 2-3 Tree T.
 {- Explanation:
@@ -41,17 +54,17 @@ add x (ThreeNode y z left middle right)
 member :: (Ord t) => t -> TwoThreeTree t -> Bool
 member x Empty = False
 member x (TwoNode y left right)
-    | x == y    = True
-    | x < y = member x left
-    | x > y = member x right
-    | otherwise = False
+  | x == y = True
+  | x < y = member x left
+  | x > y = member x right
+  | otherwise = False
 member x (ThreeNode y z left middle right)
-    | x == y    = True
-    | x == z    = True
-    | x <= y          = member x left
-    | x > y && x <= z = member x middle
-    | x > z           = member x right
-    | otherwise       = False
+  | x == y = True
+  | x == z = True
+  | x <= y = member x left
+  | x > y && x <= z = member x middle
+  | x > z = member x right
+  | otherwise = False
 
 -- Implementation of height(T), which returns the height of T.
 height :: TwoThreeTree t -> Int
@@ -60,56 +73,55 @@ height (TwoNode x left right) = (max (height left) (height right)) + 1
 height (ThreeNode x y left middle right) = (max (height left) (max (height middle) (height right))) + 1
 
 -- A main function which shows off the implementation of the functions by printing the output of the functions to standard output.
-main :: IO()
+main :: IO ()
 main = do
-    let xList = [3, 10, 15, 20, 6, 18]
+  let xList = [3, 10, 15, 20, 6, 18]
 
-    -- To show off the implementation of add(X, T).
-    -- Which returns the 2-3 Tree from adding X to the 2-3 Tree T.
-    let t1 = Empty
-    putStrLn (show (add (xList !! 0) t1))
-    let t2 = add (xList !! 0) t1
-    putStrLn (show (add (xList !! 1) t2))
-    let t3 = add (xList !! 1) t2
-    putStrLn (show (add (xList !! 2) t3))
-    let t4 = add (xList !! 2) t3
-    putStrLn (show (add (xList !! 3) t4))
-    let t5 = add (xList !! 3) t4
-    putStrLn (show (add (xList !! 4) t5))
-    let t6 = add (xList !! 4) t5
-    putStrLn (show (add (xList !! 5) t6))
+  -- To show off the implementation of add(X, T).
+  -- Which returns the 2-3 Tree from adding X to the 2-3 Tree T.
+  let t1 = Empty
+  putStrLn (show (add (xList !! 0) t1))
+  let t2 = add (xList !! 0) t1
+  putStrLn (show (add (xList !! 1) t2))
+  let t3 = add (xList !! 1) t2
+  putStrLn (show (add (xList !! 2) t3))
+  let t4 = add (xList !! 2) t3
+  putStrLn (show (add (xList !! 3) t4))
+  let t5 = add (xList !! 3) t4
+  putStrLn (show (add (xList !! 4) t5))
+  let t6 = add (xList !! 4) t5
+  putStrLn (show (add (xList !! 5) t6))
 
-    putStrLn (show ("----")) -- Just to separate the function prints.
+  putStrLn (show ("----")) -- Just to separate the function prints.
 
-    -- To show off the implementation of member(X, T).
-    -- Which returns true if X is in the 2-3 Tree T.
-    let t7 = add (xList !! 5) t6
-    putStrLn (show (member 3 t7))
-    putStrLn (show (member 10 t7))
-    putStrLn (show (member 15 t7))
-    putStrLn (show (member 20 t7))
-    putStrLn (show (member 6 t7))
-    putStrLn (show (member 18 t7))
-    putStrLn (show (member 0 t7))
-    putStrLn (show (member 100 t7))
+  -- To show off the implementation of member(X, T).
+  -- Which returns true if X is in the 2-3 Tree T.
+  let t7 = add (xList !! 5) t6
+  putStrLn (show (member 3 t7))
+  putStrLn (show (member 10 t7))
+  putStrLn (show (member 15 t7))
+  putStrLn (show (member 20 t7))
+  putStrLn (show (member 6 t7))
+  putStrLn (show (member 18 t7))
+  putStrLn (show (member 0 t7))
+  putStrLn (show (member 100 t7))
 
-    putStrLn (show ("----")) -- Just to separate the function prints.
+  putStrLn (show ("----")) -- Just to separate the function prints.
 
-    -- To show off the implementation of height(T).
-    -- Which returns the height of the 2-3 Tree.
-    let emptyTree = Empty
-    putStrLn(show (height emptyTree))
-    let binTree = TwoNode 5 (TwoNode 1 Empty (TwoNode 3 Empty Empty)) (TwoNode 7 Empty Empty)
-    putStrLn (show (height binTree))
-    putStrLn(show (height t7))
-    let t8 = add 19 t7
-    let t9 = add 19 t8
-    putStrLn(show t9)
-    let t10 = add 19 t9
-    putStrLn(show (height t10))
-
+  -- To show off the implementation of height(T).
+  -- Which returns the height of the 2-3 Tree.
+  let emptyTree = Empty
+  putStrLn (show (height emptyTree))
+  let binTree = TwoNode 5 (TwoNode 1 Empty (TwoNode 3 Empty Empty)) (TwoNode 7 Empty Empty)
+  putStrLn (show (height binTree))
+  putStrLn (show (height t7))
+  let t8 = add 19 t7
+  let t9 = add 19 t8
+  putStrLn (show t9)
+  let t10 = add 19 t9
+  putStrLn (show (height t10))
 
 {- References:
 -- Used this link to figure out how to calculate the height of a tree data structure.
 [1] https://www.digitalocean.com/community/tutorials/height-of-a-tree-data-structure
--}
+fthree-}
